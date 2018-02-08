286a70995aa9b2a0dd8365eaf0364b271f0f0659
Florian PASQUEREAU <fpasquer@e1r5p16.42.fr>
2018-02-08 13:06:08 +0100

    multi players ok

--- Client/srcs/main.cpp
+++ Client/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fpasquer <fpasquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 20:22:18 by fpasquer          #+#    #+#             */
-/*   Updated: 2018/02/08 11:52:52 by fpasquer         ###   ########.fr       */
+/*   Updated: 2018/02/08 12:58:38 by fpasquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,28 @@ int							main(int argc, char **argv)
 {
 	int						port;
 	Player_human			player1;
+	Player_human			player2;
+	Player_human			*ptr;
 
 	if (argc == 3 && (port = atoi(argv[2])) >= MIN_PORT && port < MAX_PORT)
 		player1.set_online(argv[1], port);
-	// else
-	// 	Player_human			player2;
 
 	std::string				key;
 	Grid					grid;
 	Window					win;
 
+	ptr = &player1;
 	while (1)
 	{
-		win.show(grid, player1, key);
+		win.show(grid, *ptr, key);
 		if (Key::getKey(key) == false)
 			return (-1);
-		if (player1.checkKeySelect(key) == true)
-			grid.play(player1);
+		if (ptr->checkKeySelect(key) == true)
+		{
+			grid.play(*ptr);
+			if (player1.isOnline() == false)
+				ptr = (ptr == & player1) ? &player2 : &player1;
+		}
 		if (key == KEY_ESC_)
 			break ;
 	}
