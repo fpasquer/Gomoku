fe214f752b0203d82b82b50ee427cfb5f08ddc0e
fpasquer <fpasquer@student.42.fr>
2018-02-20 21:49:29 +0100

    try hash table for perm set

--- Gomoku.hpp
+++ Gomoku.hpp
@@ -6,7 +6,7 @@
 /*   By: fpasquer <fpasquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 08:52:08 by fpasquer          #+#    #+#             */
-/*   Updated: 2018/02/19 09:43:44 by fpasquer         ###   ########.fr       */
+/*   Updated: 2018/02/20 21:46:35 by fpasquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@
 # define GET_VAL(cell) (char)cell
 # define GET_PERM(cell) cell >> 8
 # define SET_VAL(cell, val) (GET_PERM(cell) << 8) + val
-# define SET_PERM(cell, perm) ((GET_PERM(cell) | perm) << 8) + GET_VAL(cell)
+# define SET_PERM(cell, perm) ((GET_PERM(cell) ^ perm) << 8) + GET_VAL(cell)
 # define CLEAR_VAL(cell) SET_VAL(cell, 0)
 # define CLEAR_PERM(cell) (0x0 << 8) + GET_VAL(cell)
 # define CLEAR_PERM_PLAYER(cell, val, perm) SET_VAL(cell, val); SET_PERM(cell, (GET_PERM(cell) ^ perm))
+# define PERM_UP(cell, perm) (((GET_PERM(cell) & perm) == 0) ? false : true)
 
 # define SIZE_CMD 10
 # define SIZE_GRID 19
@@ -31,8 +32,8 @@
 # define INIT_SOCK -1
 # define NB_STONE_WIN 5
 # define NB_WIN_CAPTURE 10
-# define CAN_NOT_PLAY1 0x1
-# define CAN_NOT_PLAY2 0x2
+# define CAN_NOT_PLAY1 0x1 //00000001
+# define CAN_NOT_PLAY2 0x2 //00000010
 
 # define QUIT		"QUIT     "
 # define IA			"IA       "
