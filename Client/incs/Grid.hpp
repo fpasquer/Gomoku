38a98c6153660201eafc4cd1d9e64aa11f7d5684
Florian PASQUEREAU <fpasquer@e1r5p16.42.fr>
2018-02-28 13:42:47 +0100

    destroy free three ok

--- Client/incs/Grid.hpp
+++ Client/incs/Grid.hpp
@@ -6,7 +6,7 @@
 /*   By: fpasquer <fpasquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 21:30:05 by fpasquer          #+#    #+#             */
-/*   Updated: 2018/02/27 16:33:24 by fpasquer         ###   ########.fr       */
+/*   Updated: 2018/02/28 11:53:26 by fpasquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ typedef enum				e_way
 # define NB_WAY_POSSIBLE 8
 # define NB_STONE_CAPTURE 2
 # define DEPTH_FREETHREE 3
+# define DECALAGE_TAKE_OFF_CAPTURE 2
 
 class Grid
 {
@@ -64,24 +65,25 @@ class Grid
 		typedef struct		s_list_way
 		{
 			t_way			way;
-			t_way			(Grid::*f)(Player &);
+			t_way			(Grid::*f)(Player const &);
 		}					t_list_way;
 		static t_list_way const
 							m_list_way[];
 		t_way				checkCaptures(Player &player);
-		t_way				checkLeft(Player &player);
-		t_way				checkLeftTop(Player &player);
-		t_way				checkTop(Player &player);
-		t_way				checkTopRight(Player &player);
-		t_way				checkRight(Player &player);
-		t_way				checkRightBottom(Player &player);
-		t_way				checkBottom(Player &player);
-		t_way				checkBottomLeft(Player &player);
+		t_way				checkLeft(Player const &player);
+		t_way				checkLeftTop(Player const &player);
+		t_way				checkTop(Player const &player);
+		t_way				checkTopRight(Player const &player);
+		t_way				checkRight(Player const &player);
+		t_way				checkRightBottom(Player const &player);
+		t_way				checkBottom(Player const &player);
+		t_way				checkBottomLeft(Player const &player);
 		bool				play(Player_ia &player);
-		void				setUnavalable(unsigned int const y_tmp, unsigned int const x_tmp, short const val, char const unpossible);
+		void				destroyFreeThree(Player const &player);
+		void				setUnavailable(unsigned int const y_tmp, unsigned int const x_tmp, short const val);
 		void				setAvailable(unsigned int const y_tmp, unsigned int const x_tmp, short const val);
 		void				checkFreethreeAgent(unsigned int const y1, unsigned int const x1,
-				unsigned int const y2, unsigned int const x2, short const other_perm, short const other_val);
+				unsigned int const y2, unsigned int const x2, char const other_perm, char const other_val);
 		static unsigned int	m_last_x;
 		static unsigned int	m_last_y;
 		short				m_cell[SIZE_GRID][SIZE_GRID];
