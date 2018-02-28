068e060bdba23b182c47162ab680fa0d7848d95b
Arnaud MAINDRON <amaindro@e1r5p15.42.fr>
2018-02-28 15:05:08 +0100

    alignment can be countered by capture

--- Client/srcs/Grid.cpp
+++ Client/srcs/Grid.cpp
@@ -6,7 +6,7 @@
 /*   By: fpasquer <fpasquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 21:33:00 by fpasquer          #+#    #+#             */
-/*   Updated: 2018/02/28 13:42:20 by fpasquer         ###   ########.fr       */
+/*   Updated: 2018/02/28 14:57:38 by amaindro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,23 +72,90 @@ bool						Grid::haveWin(unsigned int const y,
 {
 	unsigned int			count1;
 	unsigned int			count2;
+	unsigned int			count_tmp1;
+	unsigned int			count_tmp2;
+	unsigned int			y_tmp;
+	unsigned int			x_tmp;
+	bool					countered;
+
+	countered = false;
 
 	this->countLeft(y, x, val, count1);
 	this->countRight(y, x, val, count2);
 	if (count1 + count2 + 1 >= NB_STONE_WIN)
-		return (true);
+	{
+		for (x_tmp = x - count1, y_tmp = y; countered == false && x_tmp < x + count2; x_tmp++)
+		{
+			if ((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
+					&& count_tmp1 + count_tmp2 == 1)
+				countered = true;
+			if ((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
+					&& count_tmp1 + count_tmp2 == 1)
+				countered = true;
+			if ((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
+					&& count_tmp1 + count_tmp2 == 1)
+				countered = true;
+		}
+		if (countered == false)
+			return (true);
+	}
 	this->countTop(y, x, val, count1);
 	this->countBottom(y, x, val, count2);
 	if (count1 + count2 + 1 >= NB_STONE_WIN)
-		return (true);
+	{
+		for (x_tmp = x, y_tmp = y - count1; countered == false && y_tmp < y + count2; y_tmp++)
+		{
+			if ((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
+					&& count_tmp1 + count_tmp2 == 1)
+				countered = true;
+			if ((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
+					&& count_tmp1 + count_tmp2 == 1)
+				countered = true;
+			if ((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
+					&& count_tmp1 + count_tmp2 == 1)
+				countered = true;
+		}
+		if (countered == false)
+			return (true);
+	}
 	this->countLeftTop(y, x, val, count1);
 	this->countRightBottom(y, x, val, count2);
 	if (count1 + count2 + 1 >= NB_STONE_WIN)
-		return (true);
+	{
+		for (x_tmp = x - count1, y_tmp = y - count1; countered == false && x_tmp < x + count2; x_tmp++, y_tmp++)
+		{
+			if ((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
+					&& count_tmp1 + count_tmp2 == 1)
+				countered = true;
+			if ((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
+					&& count_tmp1 + count_tmp2 == 1)
+				countered = true;
+			if ((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
+					&& count_tmp1 + count_tmp2 == 1)
+				countered = true;
+		}
+		if (countered == false)
+			return (true);
+	}
 	this->countTopRight(y, x, val, count1);
 	this->countBottomLeft(y, x, val, count2);
 	if (count1 + count2 + 1 >= NB_STONE_WIN)
-		return (true);
+	{
+		for (x_tmp = x + count1, y_tmp = y - count1; countered == false && y_tmp < y + count2; x_tmp--, y_tmp++)
+		{
+			if ((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
+					&& count_tmp1 + count_tmp2 == 1)
+				countered = true;
+			if ((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
+					&& count_tmp1 + count_tmp2 == 1)
+				countered = true;
+			if ((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
+					&& count_tmp1 + count_tmp2 == 1)
+				countered = true;
+		}
+		if (countered == false)
+			return (true);
+	}
 	return (capture.size() >= NB_WIN_CAPTURE ? true : false);
 }
 
