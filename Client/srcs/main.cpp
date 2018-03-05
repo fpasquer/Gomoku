20135e4811c0638350277e6ec622eb4e8fb2ddaf
Florian PASQUEREAU <fpasquer@e1r5p15.42.fr>
2018-03-05 07:26:23 +0100

    win player Fixed

--- Client/srcs/main.cpp
+++ Client/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fpasquer <fpasquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/05 20:22:18 by fpasquer          #+#    #+#             */
-/*   Updated: 2018/03/04 16:51:57 by fpasquer         ###   ########.fr       */
+/*   Updated: 2018/03/05 07:25:18 by fpasquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,7 +97,7 @@ int							main(int argc, char **argv)
 			break ;
 		if (player->checkKeySelect(key) == true && grid.play(*player) == true)
 		{
-			if (grid.checkIaWin(*player) == true)
+			if (grid.win(*player) == true || grid.checkIaWin(*player) == true)
 				break ;
 			if (player1.isOnline() == OFFLINE)
 				player = (player == &player1) ? &player2 : &player1;
