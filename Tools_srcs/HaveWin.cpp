d011fa2842d9c1173ed0c92ad55c224fc8f247f9
Florian PASQUEREAU <fpasquer@e1r5p15.42.fr>
2018-03-05 13:12:45 +0100

    6 alignment countered fixed

--- Tools_srcs/HaveWin.cpp
+++ Tools_srcs/HaveWin.cpp
@@ -6,7 +6,7 @@
 /*   By: fpasquer <fpasquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 11:44:05 by fpasquer          #+#    #+#             */
-/*   Updated: 2018/03/04 15:01:49 by fpasquer         ###   ########.fr       */
+/*   Updated: 2018/03/05 13:10:05 by fpasquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,82 +31,82 @@ bool						HaveWin::haveWin(unsigned int const y,
 	unsigned int			count_tmp2;
 	unsigned int			y_tmp;
 	unsigned int			x_tmp;
-	bool					countered;
+	int						countered;
 
 	this->countLeft(y, x, val, count1);
 	this->countRight(y, x, val, count2);
 	if (count1 + count2 + 1 >= NB_STONE_WIN)
 	{
-		for (countered = false, x_tmp = x - count1, y_tmp = y; countered == false && x_tmp < x + count2; x_tmp++)
+		for (countered = 1, x_tmp = x - count1, y_tmp = y; countered < NB_STONE_WIN && x_tmp < x + count2; x_tmp++, countered++)
 		{
 			if ((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1)
-				countered = true;
+				countered = 0;
 			if ((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1)
-				countered = true;
+				countered = 0;
 			if ((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1)
-				countered = true;
+				countered = 0;
 		}
-		if (countered == false)
+		if (countered >= NB_STONE_WIN)
 			return (true);
 	}
 	this->countTop(y, x, val, count1);
 	this->countBottom(y, x, val, count2);
 	if (count1 + count2 + 1 >= NB_STONE_WIN)
 	{
-		for (countered = false, x_tmp = x, y_tmp = y - count1; countered == false && y_tmp < y + count2; y_tmp++)
+		for (countered = 1, x_tmp = x, y_tmp = y - count1; countered < NB_STONE_WIN && y_tmp < y + count2; y_tmp++, countered++)
 		{
 			if ((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1)
-				countered = true;
+				countered = 0;
 			if ((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1)
-				countered = true;
+				countered = 0;
 			if ((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1)
-				countered = true;
+				countered = 0;
 		}
-		if (countered == false)
+		if (countered >= NB_STONE_WIN)
 			return (true);
 	}
 	this->countLeftTop(y, x, val, count1);
 	this->countRightBottom(y, x, val, count2);
 	if (count1 + count2 + 1 >= NB_STONE_WIN)
 	{
-		for (countered = false, x_tmp = x - count1, y_tmp = y - count1; countered == false && x_tmp < x + count2; x_tmp++, y_tmp++)
+		for (countered = 1, x_tmp = x - count1, y_tmp = y - count1; countered < NB_STONE_WIN && x_tmp < x + count2; x_tmp++, y_tmp++, countered++)
 		{
 			if ((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1)
-				countered = true;
+				countered = 0;
 			if ((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1)
-				countered = true;
+				countered = 0;
 			if ((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1)
-				countered = true;
+				countered = 0;
 		}
-		if (countered == false)
+		if (countered >= NB_STONE_WIN)
 			return (true);
 	}
 	this->countTopRight(y, x, val, count1);
 	this->countBottomLeft(y, x, val, count2);
 	if (count1 + count2 + 1 >= NB_STONE_WIN)
 	{
-		for (countered = false, x_tmp = x + count1, y_tmp = y - count1; countered == false && y_tmp < y + count2; x_tmp--, y_tmp++)
+		for (countered = 1, x_tmp = x + count1, y_tmp = y - count1; countered < NB_STONE_WIN && y_tmp < y + count2; x_tmp--, y_tmp++, countered++)
 		{
 			if ((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1)
-				countered = true;
+				countered = 0;
 			if ((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1)
-				countered = true;
+				countered = 0;
 			if ((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1)
-				countered = true;
+				countered = 0;
 		}
-		if (countered == false)
+		if (countered >= NB_STONE_WIN)
 			return (true);
 	}
 	return (capture.size() >= NB_WIN_CAPTURE ? true : false);
