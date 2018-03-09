545252d89e95e6f5a956e03d151b00406f411ec5
Arnaud MAINDRON <amaindro@e1r5p15.42.fr>
2018-03-09 11:56:36 +0100

    bug countered first/5th stone fixed

--- Tools_srcs/HaveWin.cpp
+++ Tools_srcs/HaveWin.cpp
@@ -6,7 +6,7 @@
 /*   By: fpasquer <fpasquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 11:44:05 by fpasquer          #+#    #+#             */
-/*   Updated: 2018/03/05 14:08:56 by fpasquer         ###   ########.fr       */
+/*   Updated: 2018/03/08 15:17:37 by amaindro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,17 +37,17 @@ bool						HaveWin::haveWin(unsigned int const y,
 	this->countRight(y, x, val, count2);
 	if (count1 + count2 + 1 >= NB_STONE_WIN)
 	{
-		for (countered = 1, x_tmp = x - count1, y_tmp = y; countered < NB_STONE_WIN && x_tmp < x + count2; x_tmp++, countered++)
+		for (countered = 1, x_tmp = x - count1, y_tmp = y; countered <= NB_STONE_WIN && x_tmp <= x + count2; x_tmp++, countered++)
 		{
 			if (((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = 0;
+				countered = -1;
 			if (((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID && x_tmp - (count_tmp1 + 1) < SIZE_GRID && x_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = 0;
+				countered = -1;
 			if (((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID && x_tmp + (count_tmp1 + 1) < SIZE_GRID && x_tmp - (count_tmp2 + 1) < SIZE_GRID)
-				countered = 0;
+				countered = -1;
 		}
 		if (countered >= NB_STONE_WIN)
 			return (true);
@@ -56,17 +56,17 @@ bool						HaveWin::haveWin(unsigned int const y,
 	this->countBottom(y, x, val, count2);
 	if (count1 + count2 + 1 >= NB_STONE_WIN)
 	{
-		for (countered = 1, x_tmp = x, y_tmp = y - count1; countered < NB_STONE_WIN && y_tmp < y + count2; y_tmp++, countered++)
+		for (countered = 1, x_tmp = x, y_tmp = y - count1; countered <= NB_STONE_WIN && y_tmp <= y + count2; y_tmp++, countered++)
 		{
 			if (((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && x_tmp - (count_tmp1 + 1) < SIZE_GRID && x_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = 0;
+				countered = -1;
 			if (((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID && x_tmp - (count_tmp1 + 1) < SIZE_GRID && x_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = 0;
+				countered = -1;
 			if (((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID && x_tmp + (count_tmp1 + 1) < SIZE_GRID && x_tmp - (count_tmp2 + 1) < SIZE_GRID)
-				countered = 0;
+				countered = -1;
 		}
 		if (countered >= NB_STONE_WIN)
 			return (true);
@@ -75,17 +75,17 @@ bool						HaveWin::haveWin(unsigned int const y,
 	this->countRightBottom(y, x, val, count2);
 	if (count1 + count2 + 1 >= NB_STONE_WIN)
 	{
-		for (countered = 1, x_tmp = x - count1, y_tmp = y - count1; countered < NB_STONE_WIN && x_tmp < x + count2; x_tmp++, y_tmp++, countered++)
+		for (countered = 1, x_tmp = x - count1, y_tmp = y - count1; countered <= NB_STONE_WIN && x_tmp <= x + count2; x_tmp++, y_tmp++, countered++)
 		{
 			if (((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = 0;
+				countered = -1;
 			if (((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && x_tmp - (count_tmp1 + 1) < SIZE_GRID && x_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = 0;
+				countered = -1;
 			if (((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID && x_tmp + (count_tmp1 + 1) < SIZE_GRID && x_tmp - (count_tmp2 + 1) < SIZE_GRID)
-				countered = 0;
+				countered = -1;
 		}
 		if (countered >= NB_STONE_WIN)
 			return (true);
@@ -94,17 +94,17 @@ bool						HaveWin::haveWin(unsigned int const y,
 	this->countBottomLeft(y, x, val, count2);
 	if (count1 + count2 + 1 >= NB_STONE_WIN)
 	{
-		for (countered = 1, x_tmp = x + count1, y_tmp = y - count1; countered < NB_STONE_WIN && y_tmp < y + count2; x_tmp--, y_tmp++, countered++)
+		for (countered = 1, x_tmp = x + count1, y_tmp = y - count1; countered <= NB_STONE_WIN && x_tmp >= x - count2; x_tmp--, y_tmp++, countered++)
 		{
 			if (((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = 0;
+				countered = -1;
 			if (((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID && x_tmp - (count_tmp1 + 1) < SIZE_GRID && x_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = 0;
+				countered = -1;
 			if (((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && x_tmp - (count_tmp1 + 1) < SIZE_GRID && x_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = 0;
+				countered = -1;
 		}
 		if (countered >= NB_STONE_WIN)
 			return (true);
