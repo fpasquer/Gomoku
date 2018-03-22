46178e82d3f31cf1af0ca47cd8b7d0409ff67198
Arnaud MAINDRON <amaindro@e1r5p14.42.fr>
2018-03-22 16:32:26 +0100

    weird issue on top left fixed, countered weight added

--- Tools_srcs/HaveWin.cpp
+++ Tools_srcs/HaveWin.cpp
@@ -6,7 +6,7 @@
 /*   By: fpasquer <fpasquer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 11:44:05 by fpasquer          #+#    #+#             */
-/*   Updated: 2018/03/20 14:20:18 by amaindro         ###   ########.fr       */
+/*   Updated: 2018/03/22 16:24:59 by amaindro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@
 bool						HaveWin::haveWin(unsigned int const y,
 		unsigned int const x, short const val, std::string const &capture, int *weight) const
 {
+	bool					ret;
 	unsigned int			count1;
 	unsigned int			count2;
 	unsigned int			count_tmp1;
@@ -34,103 +35,87 @@ bool						HaveWin::haveWin(unsigned int const y,
 	int						countered;
 	int						tmp_weight = 0;
 
-	this->countLeft(y, x, val, count1);
-	this->countRight(y, x, val, count2);
-	if (count1 + count2 + 1 >= NB_STONE_WIN)
+	ret = this->countLeft(y, x, val, count1) | this->countRight(y, x, val, count2);
+	for (countered = 0, x_tmp = x - count1, y_tmp = y; countered <= NB_STONE_WIN - 1 && x_tmp <= x + count2; x_tmp++, countered++)
 	{
-		for (countered = 1, x_tmp = x - count1, y_tmp = y; countered <= NB_STONE_WIN && x_tmp <= x + count2; x_tmp++, countered++)
-		{
-			if (((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
+		if (((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = -1;
-			if (((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
+			countered = -1;
+		if (((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID && x_tmp - (count_tmp1 + 1) < SIZE_GRID && x_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = -1;
-			if (((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
+			countered = -1;
+		if (((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID && x_tmp + (count_tmp1 + 1) < SIZE_GRID && x_tmp - (count_tmp2 + 1) < SIZE_GRID)
-				countered = -1;
-		}
-		if (countered >= NB_STONE_WIN)
-			return (true);
+			countered = -1;
 	}
-		//setting the alignment weight
-		if (tmp_weight < (int)(count1 + count2 + 1))
-			tmp_weight = count1 + count2 + 1;
+	if (countered >= NB_STONE_WIN)
+		return (true);
+	//setting the alignment weight
+	if (tmp_weight < countered * ret)
+		tmp_weight = countered;
 
-	this->countTop(y, x, val, count1);
-	this->countBottom(y, x, val, count2);
-	if (count1 + count2 + 1 >= NB_STONE_WIN)
+	ret = this->countTop(y, x, val, count1) | this->countBottom(y, x, val, count2);
+	for (countered = 0, x_tmp = x, y_tmp = y - count1; countered <= NB_STONE_WIN - 1 && y_tmp <= y + count2; y_tmp++, countered++)
 	{
-		for (countered = 1, x_tmp = x, y_tmp = y - count1; countered <= NB_STONE_WIN && y_tmp <= y + count2; y_tmp++, countered++)
-		{
-			if (((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
+		if (((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && x_tmp - (count_tmp1 + 1) < SIZE_GRID && x_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = -1;
-			if (((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
+			countered = -1;
+		if (((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID && x_tmp - (count_tmp1 + 1) < SIZE_GRID && x_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = -1;
-			if (((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
+			countered = -1;
+		if (((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID && x_tmp + (count_tmp1 + 1) < SIZE_GRID && x_tmp - (count_tmp2 + 1) < SIZE_GRID)
-				countered = -1;
-		}
-		if (countered >= NB_STONE_WIN)
-			return (true);
+			countered = -1;
 	}
-		//setting the alignment weight
-		if (tmp_weight < (int)(count1 + count2 + 1))
-			tmp_weight = count1 + count2 + 1;
+	if (countered >= NB_STONE_WIN)
+		return (true);
+	//setting the alignment weight
+	if (tmp_weight < countered * ret)
+		tmp_weight = countered;
 
-	this->countLeftTop(y, x, val, count1);
-	this->countRightBottom(y, x, val, count2);
-	if (count1 + count2 + 1 >= NB_STONE_WIN)
+	ret = this->countLeftTop(y, x, val, count1) | this->countRightBottom(y, x, val, count2);
+	for (countered = 0, x_tmp = x - count1, y_tmp = y - count1; countered <= NB_STONE_WIN - 1 && x_tmp <= x + count2; x_tmp++, y_tmp++, countered++)
 	{
-		for (countered = 1, x_tmp = x - count1, y_tmp = y - count1; countered <= NB_STONE_WIN && x_tmp <= x + count2; x_tmp++, y_tmp++, countered++)
-		{
-			if (((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
+		if (((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = -1;
-			if (((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
+			countered = -1;
+		if (((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && x_tmp - (count_tmp1 + 1) < SIZE_GRID && x_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = -1;
-			if (((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
+			countered = -1;
+		if (((this->countTopRight(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottomLeft(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID && x_tmp + (count_tmp1 + 1) < SIZE_GRID && x_tmp - (count_tmp2 + 1) < SIZE_GRID)
-				countered = -1;
-		}
-		if (countered >= NB_STONE_WIN)
-			return (true);
+			countered = -1;
 	}
-		//setting the alignment weight
-		if (tmp_weight < (int)(count1 + count2 + 1))
-			tmp_weight = count1 + count2 + 1;
+	if (countered >= NB_STONE_WIN)
+		return (true);
+	//setting the alignment weight
+	if (tmp_weight < countered * ret)
+		tmp_weight = countered;
 
-	this->countTopRight(y, x, val, count1);
-	this->countBottomLeft(y, x, val, count2);
-	if (count1 + count2 + 1 >= NB_STONE_WIN)
+	ret = this->countTopRight(y, x, val, count1) | this->countBottomLeft(y, x, val, count2);
+	for (countered = 0, x_tmp = x + count1, y_tmp = y - count1; countered <= NB_STONE_WIN - 1 && x_tmp >= x - count2 && x_tmp < SIZE_GRID; x_tmp--, y_tmp++, countered++)
 	{
-		for (countered = 1, x_tmp = x + count1, y_tmp = y - count1; countered <= NB_STONE_WIN && x_tmp >= x - count2; x_tmp--, y_tmp++, countered++)
-		{
-			if (((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
+		if (((this->countTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = -1;
-			if (((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
+			countered = -1;
+		if (((this->countLeftTop(y_tmp, x_tmp, val, count_tmp1) ^ this->countRightBottom(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && y_tmp - (count_tmp1 + 1) < SIZE_GRID && y_tmp + (count_tmp2 + 1) < SIZE_GRID && x_tmp - (count_tmp1 + 1) < SIZE_GRID && x_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = -1;
-			if (((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
+			countered = -1;
+		if (((this->countLeft(y_tmp, x_tmp, val, count_tmp1) ^ this->countRight(y_tmp, x_tmp, val, count_tmp2))
 					&& count_tmp1 + count_tmp2 == 1) && x_tmp - (count_tmp1 + 1) < SIZE_GRID && x_tmp + (count_tmp2 + 1) < SIZE_GRID)
-				countered = -1;
-		}
-		if (countered >= NB_STONE_WIN)
-			return (true);
+			countered = -1;
 	}
-		//setting the alignment weight
-		if (tmp_weight < (int)(count1 + count2 + 1))
-			tmp_weight = count1 + count2 + 1;
+	if (countered >= NB_STONE_WIN)
+		return (true);
+	//setting the alignment weight
+	if (tmp_weight < countered * ret)
+		tmp_weight = countered;
 
 	if (weight != NULL)
 	{
 		switch (tmp_weight)
 		{
-			case 4 ... SIZE_GRID : *weight = 50;
+			case 4 ... SIZE_GRID : *weight = 150;
 					break ;
 			case 3: *weight = 10;
 					break ;
